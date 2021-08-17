d3f290fc7440ad50ec0adff22bba492c1ab8dfd3
qiqinglin <qiqinglin7@gmail.com>
2021-08-17 15:08:46 +0800

    优化首页跳转逻辑

--- debug/src/main/java/com/example/debug/MainActivity.kt
+++ debug/src/main/java/com/example/debug/MainActivity.kt
@@ -3,20 +3,27 @@ package com.example.debug
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.common.Constant
 import com.example.common.service.ServiceManager
 import com.zxf.basic.expand.FindView
+import com.zxf.basic.utils.MMKVUtils
 
 class MainActivity : AppCompatActivity() {
     private val tvLogin: TextView by FindView(R.id.tvLogin)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_debug)
-        tvLogin.setOnClickListener {
-            ServiceManager.startLogin(this) {
+        val isLogin = MMKVUtils.get().get<Boolean>(Constant.KEY_LOGIN)
+        if (isLogin == true) {
+            ServiceManager.startMain(this)
+        } else {
+            setContentView(R.layout.activity_debug)
+            tvLogin.setOnClickListener {
+                ServiceManager.startLogin(this) {
 
+                }
+                finish()
             }
-            finish()
         }
     }
 
